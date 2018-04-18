c1cc06223fa09074b1f60699c1b6bc9d191efcc4
kinogam <kinogam@gmail.com>
2018-04-19 00:09:28 +0800

    add jest es6 sample

--- src/hello-component/hello.js
+++ src/hello-component/hello.js
@@ -1,6 +1,6 @@
 import './hello.css';
-import * as html from './hello.html';
+import html from './hello.html';
 
-export function sayHello(name: string): string {
+export function sayHello(name){
     return html.replace(/[\s\r\n]*<[^>]+>[\s\r\n]*/g, '').replace('world', name) + '!';
 }
